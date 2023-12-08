c168dbaf9abbe0c3113dba813cd25b5657cd1a08
NillRudd <nroslund@kth.se>
2023-12-08 13:57:04 +0100

    new test

--- image-service/server.js
+++ image-service/server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'host.docker.internal',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'wiljamniklas',
     database: process.env.DB_NAME || 'images',
